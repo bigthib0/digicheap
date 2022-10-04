18c85b71e4f22307660a12070908ebdb7fe794ed
Thibaut Michaud <thibaut-michaud@hotmail.ch>
2022-10-04 16:07:33 +0200

    add default route

--- digicheap.py
+++ digicheap.py
@@ -111,6 +111,10 @@ def update_product_price(url):
     
     return price_history
 
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
